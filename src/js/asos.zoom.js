a2e4a2b74d581d0da42eacf8d98c25ca4087b08e
Krzysztof Lojniewski <kontakt@pagepro.pl>
2015-05-02 22:25:51 +0200

    rewrite version 2 (complete)

--- src/js/asos.zoom.js
+++ src/js/asos.zoom.js
@@ -1,35 +1,38 @@
 /*globals jQuery, google, document */
 (function ($) {
     "use strict";
-    var pluginName  =   "asosZoom",
-        defaults    =   {
+    var pluginName = "asosZoom",
+        defaults = {
             zoom: 'horizontal',
             speed: 500,
             thumbnails: true,
-            closeButton: true,
+            closeButtonTemplate: '<a href="#" id="asosZoom__content__close">{{closeText}}</a>',
             closeText: 'x',
+            arrowsTemplate: '<a href="#" id="asosZoom__content__next">{{nextText}}</a><a href="#" id="asosZoom__content__prev">{{prevText}}</a>',
             thumbnailPosition: 'vertical',
-            onStart: function () {},
-            onComplete: function () {},
-            onCleanup: function () {},
+            onDisplayed: function () {},
             onClose: function () {},
             overlayColor: '#000000',
             overlayOpacity: '0.7',
             imageResize: null,
             containerWidth: '70%',
             containerHeight: '90%',
-            nextText : '&gt;',
-            prevText : '&lt;',
-            nav : true
+            nextText: '&gt;',
+            prevText: '&lt;',
+            nav: true
         };
     // The actual plugin constructor
     function Plugin(element, options) {
-        this.element        =   element;
-        this.$element       =   $(this.element);
-        this.$body          =   $('body');
-        this.options        =   options;
-        this.metadata       =   this.$element.data('options');
-        this.settings       =   $.extend({}, defaults, this.options, this.metadata);
+        this.element = element;
+        this.$element = $(this.element);
+        this.$thumbs = null;
+        this.$galleryLinks = null;
+        this.$body = $('body');
+        this.$zoomedImage = null;
+        this.options = options;
+        this.metadata = this.$element.data('options');
+        this.settings = $.extend({}, defaults, this.options, this.metadata);
+        this.currentIndex = 0;
         this.init();
     }
     Plugin.prototype = {
@@ -41,11 +44,14 @@
             var that = this;
             that.$element.on('click', function (e) {
                 e.preventDefault();
-                that.removeZoom();
-                that.appendZoomHtml($(this).attr('href'), $(this).attr('rel'));
+                that.removeZoom(true);// remove previous instance
+                that.appendZoomHtml($(this).attr('href'), $(this).attr('rel'));// append zoom
                 that.showZoom();
                 that.enableZoomActions();
                 that.closingActions();
+                that.arrows();
+                that.updateCurrentThumb();
+                that.settings.onDisplayed.call(that);
             });
         },
         showZoom: function () {
@@ -54,8 +60,11 @@
                 .fadeTo(0, this.settings.overlayOpacity);
             $('#asosZoom').fadeIn(this.settings.speed);
         },
-        removeZoom: function () {
+        removeZoom: function (cleanUp) {
             $('#asosZoom').remove();
+            if (!cleanUp) {
+                this.settings.onClose.call(this);
+            }
         },
         closingActions: function () {
             var that = this;
@@ -70,9 +79,9 @@
         },
         enableZoomActions: function () {
             var that = this;
-            $('#asosZoom__content__img__xl').on('mousemove', function (e) {
-                var fullWidth = $('#asosZoom__content__img__xl').width(),
-                    fullHeight = $('#asosZoom__content__img__xl').height(),
+            that.$zoomedImage.on('mousemove', function (e) {
+                var fullWidth = that.$zoomedImage.width(),
+                    fullHeight = that.$zoomedImage.height(),
                     contentWidth = $('#asosZoom__content').width(),
                     contentHeight = $('#asosZoom__content').height(),
                     offset = $('#asosZoom__content').offset(),
@@ -81,11 +90,14 @@
                     posX = (Math.round((mouseX / contentWidth) * 100) / 100) * (fullWidth - contentWidth),
                     posY = (Math.round((mouseY / contentHeight) * 100) / 100) * (fullHeight - contentHeight);
                 if (that.settings.zoom === 'vertical') {
-                    $('#asosZoom__content__img__xl').css('top', '-' + posY + 'px');
+                    that.$zoomedImage.css('top', '-' + posY + 'px');
                 } else if (that.settings.zoom === 'horizontal') {
-                    $('#asosZoom__content__img__xl').css('left', '-' + posX + 'px');
+                    that.$zoomedImage.css('left', '-' + posX + 'px');
                 } else {
-                    $('#asosZoom__content__img__xl').css({'top': '-' + posY + 'px', 'right': '' + posX + 'px'});
+                    that.$zoomedImage.css({
+                        top: '-' + posY + 'px',
+                        right: posX + 'px'
+                    });
                 }
             });
         },
@@ -115,28 +127,79 @@
             }
             this.$body.append(zoomHtml);
             $('#' + imageContainer).append(zoomedImage);
-            console.log(zoomHtml);
-            console.log($('#' + imageContainer));
             if (imageRel && this.settings.nav) {
                 this.appendThumbnails(imageRel);
             }
-            if (this.settings.closeButton) {
-                var closeBtn = '<a href="#" id="asosZoom__content__close">' + this.settings.closeText + '</a>';
-                $('#asosZoom__content').append(closeBtn);
-            }
+            $('#asosZoom__content').append(this.settings.closeButtonTemplate.replace('{{closeText}}', this.settings.closeText));
+            this.$zoomedImage = $('#asosZoom__content__img__xl');
         },
         appendThumbnails: function (imageRel) {
-            var $thumbs = $('a[rel="' + imageRel + '"]');
-            if ($thumbs.length > 1) {
-                this.appendArrows();
-                $thumbs.each(function () {
+            var that = this;
+            that.$galleryLinks = $('a[rel="' + imageRel + '"]');
+            if (that.$galleryLinks.length > 1) {
+                that.appendArrows();
+                that.$galleryLinks.each(function (index) {
+                    if (this === that.element) {
+                        that.currentIndex = index;
+                    }
                     $('#asosZoom__content__thumbs').append('<a href="' + this.href + '" class="asosZoom-thumb"><img src="' + this.href + '" alt=""/></a>');
+                });
+                that.$thumbs = $('#asosZoom__content__thumbs a');
+                that.$thumbs.on('click', function (e) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    var $this = $(this);
+                    $this.siblings().removeClass('active');
+                    $this.addClass('active');
+                    that.currentIndex = that.$thumbs.index($this);
+                    that.$zoomedImage.attr('src', $this.attr('href'));
                 });
             }
         },
+        arrows: function () {
+            var that = this;
+            $('#asosZoom__content__next').on('click', function (e) {
+                e.preventDefault();
+                e.stopPropagation();
+                that.showNext();
+            });
+            $('#asosZoom__content__prev').on('click', function (e) {
+                e.preventDefault();
+                e.stopPropagation();
+                that.showPrev();
+            });
+            $(document).off('keydown.arrows').on('keydown.arrows', function (e){
+                if (e.keyCode === 37) {
+                    that.showPrev();
+                } else if (e.keyCode === 39) {
+                    that.showNext();
+                }
+            });
+        },
         appendArrows: function () {
-            var buttons = '<a href="#" id="asosZoom__content__next">' + this.settings.nextText + '</a><a href="#" id="asosZoom__content__prev">' + this.settings.prevText + '</a>';
-            $('#asosZoom__content').append(buttons);
+            $('#asosZoom__content').append(this.settings.arrowsTemplate.replace('{{nextText}}', this.settings.nextText).replace('{{prevText}}', this.settings.prevText));
+        },
+        showNext: function () {
+            if (this.currentIndex + 1 >= this.$thumbs.length) {
+                this.currentIndex = 0;
+            } else {
+                this.currentIndex += 1;
+            }
+            this.$zoomedImage.attr('src', this.$thumbs[this.currentIndex].href);
+            this.updateCurrentThumb();
+        },
+        showPrev: function () {
+            if (this.currentIndex <= 0) {
+                this.currentIndex = this.$thumbs.length - 1;
+            } else {
+                this.currentIndex -= 1;
+            }
+            this.$zoomedImage.attr('src', this.$thumbs[this.currentIndex].href);
+            this.updateCurrentThumb();
+        },
+        updateCurrentThumb: function () {
+            this.$thumbs.removeClass('active');
+            this.$thumbs.eq(this.currentIndex).addClass('active');
         }
     };
     $.fn[pluginName] = function (options) {
